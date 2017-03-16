e87656cd68c6e0124f640e1ff13baf78395f1df8
Rajasimman S <srajasimman@gmail.com>
2017-03-16 15:03:11 +0530

    choco script added

--- Uninstall Windows 10 Apps.ps1
+++ Uninstall Windows 10 Apps.ps1
@@ -16,8 +16,8 @@ Get-AppxPackage *onenote* | Remove-AppxPackage
 Get-AppxPackage *people* | Remove-AppxPackage
 Get-AppxPackage *windowsphone* | Remove-AppxPackage
 Get-AppxPackage *photos* | Remove-AppxPackage
-Get-AppxPackage *windowsstore* | Remove-AppxPackage
 Get-AppxPackage *bingsports* | Remove-AppxPackage
 Get-AppxPackage *soundrecorder* | Remove-AppxPackage
 Get-AppxPackage *bingweather* | Remove-AppxPackage
 Get-AppxPackage *xboxapp* | Remove-AppxPackage
+
